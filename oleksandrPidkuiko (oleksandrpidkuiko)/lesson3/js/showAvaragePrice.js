64bbd6c55fa4f90adac21ea2059f396cb7016621
Oleksandr Pidkuiko <o.pidkuiko111@gmail.com>
2018-10-05 20:18:39 +0300

    Added index.html, which include destructuringJSON.js and showAvaragePrice.js. destructuringJSON.js implements the objects de-structuring.

---
+++ oleksandrPidkuiko (oleksandrpidkuiko)/lesson3/js/showAvaragePrice.js
@@ -0,0 +1,83 @@
+'use strict'
+
+let cars = [
+    {
+        "id": 1,
+        "name": "Sprinter 2500",
+        "firstPrice": 8172,
+        "secondPrice": 9273
+    },
+    {
+        "id": 2,
+        "name": "Touareg",
+        "firstPrice": 5856,
+        "secondPrice": 5130
+    },
+    {
+        "id": 3,
+        "name": "Avenger",
+        "firstPrice": 8674,
+        "secondPrice": 3118
+    },
+    {
+        "id": 4,
+        "name": "Tracer",
+        "firstPrice": 8406,
+        "secondPrice": 2382
+    },
+    {
+        "id": 5,
+        "name": "Durango",
+        "firstPrice": 2093,
+        "secondPrice": 6622
+    },
+    {
+        "id": 6,
+        "name": "968",
+        "firstPrice": 7735,
+        "secondPrice": 3681
+    },
+    {
+        "id": 7,
+        "name": "Golf",
+        "firstPrice": 9363,
+        "secondPrice": 7039
+    },
+    {
+        "id": 8,
+        "name": "Z4",
+        "firstPrice": 4827,
+        "secondPrice": 6776
+    },
+    {
+        "id": 9,
+        "name": "LaCrosse",
+        "firstPrice": 3169,
+        "secondPrice": 5229
+    },
+    {
+        "id": 10,
+        "name": "Insight",
+        "firstPrice": 9030,
+        "secondPrice": 9207
+    }
+];
+
+function average() {
+    let sum = 0;
+    for (let i = 0; i <arguments.length; i++ ){
+        sum += arguments[i];
+    }
+    return sum === 0 ? sum : sum / arguments.length;
+}
+
+let newCars = cars.map(function (currentValue) {
+    return {
+        'id': currentValue.id,
+        'name': currentValue.name,
+        'avaragePriceOfCar': average(currentValue.firstPrice, currentValue.secondPrice)
+    }
+});
+
+
+
